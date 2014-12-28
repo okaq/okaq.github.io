fcf6cca88341dc084297a24540ce53aa96678f9a
AQ <aq@okaq.com>
2014-12-28 17:53:31 -0500

    Hana render test fix

--- web.go
+++ web.go
@@ -9,6 +9,7 @@ import (
 
 const (
     HTML = "index.html"
+    HANA2D = "hana2d.js"
     PORT = ":8008"
 )
 
@@ -16,6 +17,10 @@ func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Println(r)
         http.ServeFile(w, r, HTML)
+    })
+    http.HandleFunc("/hana2d.js", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Println(r)
+        http.ServeFile(w, r, HANA2D)
     })
     fmt.Println("Starting okaq.github.io dev web server on localhost" + PORT)
     http.ListenAndServe(PORT, nil)

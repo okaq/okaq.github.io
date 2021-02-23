dfec0a7e20ae41e31696807320e0f3d7b0aad6dd
aq <aq@okaq.com>
2021-02-23 08:31:51 -0500

    New file tactic.html

---
+++ x/b/tactic.go
@@ -0,0 +1,32 @@
+// bacterial tactics
+// okaq nano game
+// AQ <aq@okaq.com>
+// 2021-02-23
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const (
+	INDEX = "tactic.html"
+)
+
+func motd() {
+	fmt.Println(time.Now().String())
+	fmt.Println("tactics live localhost:8080")
+}
+
+func TacticHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w,r,INDEX)
+}
+
+func main() {
+	motd()
+	http.HandleFunc("/", TacticHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+
